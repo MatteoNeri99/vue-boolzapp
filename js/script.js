22cc7b07eb549ab58cea65dc282ee45466db243a
MatteoNeri99 <matteolavoro99@gmail.com>
2024-05-31 10:42:55 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+const { createApp } = Vue
+
+
+createApp({
+  data() {
+    return {
+
+       
+             
+
+            
+       
+   
+      
+    }
+  },
+
+  methods:{
+
+
+    
+  }
+}).mount('#app')
